747d4485c97ae4716367334153ed0d724849eceb
Michael Munro <mlmunro@gmail.com>
2022-11-09 21:44:40 +1100

    sg

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "ec2_instance" {
 
   ami                    = "ami-02a66f06b3557a897" #AL2
   instance_type          = "t2.micro"
-  key_name               = "user1"
+  key_name               = "mike-sshkey-webservices"
   monitoring             = true
   vpc_security_group_ids = ["sg-0c689f0ed9600d404"]
   subnet_id              = "subnet-01becfe4d413783c4"
